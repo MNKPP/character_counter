79b32c1d08552df342da5bf3691351929c1ca0de
Mike Godfrinne <mike.godfrinne.dev@gmail.com>
2025-02-02 19:49:53 +0800

    Add configuration for css module, make the css for dark mode switch

--- src/components/DarkMode.jsx
+++ src/components/DarkMode.jsx
@@ -1,9 +1,16 @@
+import s from './DarkMode.module.scss';
+import IconSun from '../assets/images/icon-sun.svg';
+import IconMoon from '../assets/images/icon-moon.svg';
+
 function DarkMode({isDarkMode, onDarkMode}) {
 
+    const iconSrc = isDarkMode ? IconMoon : IconSun;
+    const alt = isDarkMode ? 'Icon Moon' : 'Icon Sun';
+
     return (
-        <button onClick={() => onDarkMode()}>
-            Sun Icon
-            <img src="" alt=""/>
+        <button className={`${isDarkMode ? s.darkModeSwitchLight : s.darkModeSwitchDark} ${s.darkModeSwitch}`}
+                onClick={() => onDarkMode()}>
+            <img src={iconSrc} alt={alt}/>
         </button>
     )
 }
