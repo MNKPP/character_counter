4bc33065ecb88ea5b30f3db554ccec5b0f02026c
Mike Godfrinne <mike.godfrinne.dev@gmail.com>
2025-02-05 08:58:42 +0800

    Add variant support and dynamic styles to CounterBlock
    Updated CounterBlock to support a `variant` prop with dynamic styles for purple, orange, and yellow themes. Refactored the SCSS to apply background colors based on the variant and updated TextCounter for specific usage with appropriate variants and images.

--- src/components/CounterBlock.jsx
+++ src/components/CounterBlock.jsx
@@ -1,13 +1,25 @@
 import s from './CounterBlock.module.scss';
 
-function CounterBlock() {
+function CounterBlock({src, alt, variant}) {
+
+    function variantColor() {
+        switch (variant) {
+            case 'purple':
+                return s.purple;
+            case 'orange':
+                return s.orange;
+            case 'yellow':
+                return s.yellow;
+        }
+    }
+
     return (
-        <div className={s.counterBlock}>
+        <div className={`${s.counterBlock} ${variantColor()}`}>
             <div>
                 <span>278</span>
                 <p>Total Characters</p>
             </div>
-            <img src="./src/assets/images/pattern-character-count.svg" alt="pattern"/>
+            <img src={src} alt="alt"/>
         </div>
     )
 }
--- src/containers/TextCounter.jsx
+++ src/containers/TextCounter.jsx
@@ -3,9 +3,21 @@ import CounterBlock from "../components/CounterBlock.jsx";
 function TextCounter() {
     return (
         <div className="text-counter">
-            <CounterBlock />
-            <CounterBlock />
-            <CounterBlock />
+            <CounterBlock
+                src="./src/assets/images/pattern-character-count.svg"
+                alt="Pattern character count"
+                variant="purple"
+            />
+            <CounterBlock
+                src="./src/assets/images/pattern-word-count.svg"
+                alt="Pattern word count"
+                variant="yellow"
+            />
+            <CounterBlock
+                src="./src/assets/images/pattern-sentence-count.svg"
+                alt="Pattern sentence count"
+                variant="orange"
+            />
         </div>
     )
 }
